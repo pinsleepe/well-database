46d644fc78605ea1f178f47b410de5122f38b10b
Mono <mkobrocka@gmail.com>
2020-12-29 12:46:01 +0200

    bacis model

--- database/models.py
+++ database/models.py
@@ -1,29 +1,28 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy import (Column, Integer, String, ForeignKey, Boolean)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref, sessionmaker, joinedload
+from sqlalchemy.orm import relationship
+from database import db
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = db.engine
 
-# The base class which our objects will be defined on.
 Base = declarative_base()
+Base.metadata.create_all(engine)
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
 
-    # Every SQLAlchemy table should have a primary key named 'id'
     id = Column(Integer, primary_key=True)
 
-    # contact fields
     arm_id = Column(Integer, ForeignKey('arm.id'))
 
     # demographic info
-    # start
+    # start of intervention
     gender = Column(String)
     sexual_orientation = Column(String)
     partnership_status = Column(String)
     mental_health = Column(String)
-    # end
+    # end of intervention
     age = Column(Integer)
     socioeconomic_status = Column(String)
     household_composition = Column(String)
@@ -41,40 +40,6 @@ class Arm(Base):
     users = relationship("user")
 
 
-class Quiz(Base):
-    __tablename__ = 'quiz'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    questions = relationship("QuizQuestion")
-
-
-class QuizQuestion(Base):
-    __tablename__ = 'quiz_question'
-
-    id = Column(Integer, primary_key=True)
-    quiz_id = Column(Integer, ForeignKey('quiz.id'))
-    text = Column(String)
-    options = relationship("QuizQuestionOption")
-
-
-class QuizQuestionOption(Base):
-    __tablename__ = 'quiz_question_option'
-
-    id = Column(Integer, primary_key=True)
-    quiz_question_id = Column(Integer, ForeignKey('quiz_question.id'))
-    text = Column(String)
-
-
-class QuizUserAnswer(Base):
-    __tablename__ = 'quiz_user_answer'
-
-    id = Column(Integer, primary_key=True)
-    quiz_question_id = Column(Integer, ForeignKey('quiz_question.id'))
-    quiz_question_option_id = Column(Integer, ForeignKey('quiz_question_option.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-
 class Module(Base):
     __tablename__ = 'module'
 
@@ -89,6 +54,8 @@ class Flow(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     questions = relationship("FlowQuestion")
+    # this relates specifically to quizzes at the end of a module
+    is_quiz = Column(Boolean)
     # TODO PC group will not complete any module so we can add a dummy one
     # so that the database don't brake, like version PC?
     module_id = Column(Integer, ForeignKey('module.id'))
@@ -116,6 +83,12 @@ class FlowUserAnswer(Base):
 
     id = Column(Integer, primary_key=True)
     flow_question_id = Column(Integer, ForeignKey('flow_question.id'))
+    # user answer
     flow_question_option_id = Column(Integer, ForeignKey('flow_question_option.id'))
     user_id = Column(Integer, ForeignKey('user.id'))
 
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine,
+                             Base.metadata.tables.values(),
+                             checkfirst=True)
